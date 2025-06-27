bbb86879b837ed6c55d3bd036c5ff76237028af1
Iwancof <rock.iwancof@gmail.com>
2025-06-27 21:26:50 +0900

    add template

---
+++ template/main.c
@@ -0,0 +1,15 @@
+#define _GNU_SOURCE
+
+#include <kpwn/prelude.h>
+
+int main(int argc, char* argv[]) {
+  noaslr(argc, argv);
+  log_level = LOG_DEBUG;
+  hexdump_width = 16;
+
+  init_billy();
+
+  log_info("pwning started");
+
+  interactive();
+}
